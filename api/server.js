1b01faa04e046253e8cdd446b475a5b8770da400
Quang Vinh <giusevinh12@gmail.com>
2023-05-13 09:38:07 +0700

    add cors

--- api/server.js
+++ api/server.js
@@ -6,6 +6,11 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 // Add this before server.use(router)
 server.use(router)
 server.listen(3000, () => {
