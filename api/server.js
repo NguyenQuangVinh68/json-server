21c671b7672e069f6cb291400f2da024ee28d41e
Quang Vinh <giusevinh12@gmail.com>
2023-05-13 09:43:59 +0700

    change url in cors

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
     res.header('Access-Control-Allow-Headers', '*')
     next()
 })
